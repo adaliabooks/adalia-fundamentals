01d60774bd8c6937fa2016d46ac5f7c08f0427d1
adaliabooks <adaliabooks@gmail.com>
2017-09-29 14:00:38 +0100

    Updated version and URLs

--- adalia-fundamentals.meta.js
+++ adalia-fundamentals.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Adalia Fundamentals
 // @namespace    https://gist.github.com/adaliabooks/
-// @version      1.8.4
+// @version      2.0.0
 // @description  A package of fixes, new features and changes to improve GoG.com. I fix GoG, so you don't have to!
 // @author       adaliabooks
-// @updateURL    https://github.com/adaliabooks/adalia-fundamentals/raw/master/adalia-fundamentals.meta.js
-// @downloadURL  https://github.com/adaliabooks/adalia-fundamentals/raw/master/adalia-fundamentals.user.js
+// @updateURL    https://github.com/adaliabooks/adalia-fundamentals/raw/development/adalia-fundamentals.meta.js
+// @downloadURL  https://github.com/adaliabooks/adalia-fundamentals/raw/development/adalia-fundamentals.user.js
 // @include      http://www.gog.com/*
 // @include      https://www.gog.com/*
 // @include      http://chat.gog.com/*
